456a88f5e0a6c3e3238e8a734b144896c22fa16e
sahil-katle <sahilkatle5@gmail.com>
2024-04-05 20:10:39 +0530

    Added profiles viewset, permissions, authentication

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from profiles_api import serializers
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+
+from profiles_api import serializers
 from profiles_api import models
+from profiles_api import permissions
 
 # Create your views here.
 class HelloApiView(APIView):
@@ -78,5 +82,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating of profiles"""
     serializer_class = serializers.UserProfileSerializer
     queryset= models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
 
-    
